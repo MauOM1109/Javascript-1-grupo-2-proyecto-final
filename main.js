20bb66b4889b2501e1f55baa631ac468b8aaa36c
Mauricio Orozco Mora <106451217+MauOM1109@users.noreply.github.com>
2022-12-07 23:46:39 -0600

    Mauricio + Bradley
    Mauricio + Bradley

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
+/* Mau */
+let data = [];
 
+function deleteRow(r) {
+
+  let i = r.parentNode.parentNode.rowIndex;
+  data.splice(i - 1, 1);
+  document.getElementById("tbProductos").deleteRow(i);
+
+}
+
+function imprimirProductos() {
+  crearObjetoHTMLProductos();
+}
+
+function crearObjetoHTMLProductos() {
+  let productos = {};
+  const tbProductos = document.getElementById('tbProductos');
+  const producto = document.getElementById('nombre');
+  const precio = document.getElementById('precio');
+  const descripcion = document.getElementById('descripcion');
+  const sel = document.getElementById('categoria');
+  const categoria = sel.options[sel.selectedIndex].text;
+  const cantidad = document.getElementById('cantidad');
+}
+/* Ali */
+  productos = {
+    Producto: producto.value,
+    Precio: precio.value,
+    Descripcion: descripcion.value,
+    Categoria: categoria,
+    Cantidad: cantidad.value
+  }
+  let dataFilter = data.filter(x => x.Producto == productos.Producto);
+
+  if (dataFilter.length == 0) {
+    data.push(productos);
+  }
